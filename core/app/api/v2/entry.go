456a1579d320921e2b799311722020797087cc65
alexistb2904 <59259007+alexistb2904@users.noreply.github.com>
2026-08-07 19:35:16 +0200

    use concrete access service for security extensions

--- core/app/api/v2/entry.go
+++ core/app/api/v2/entry.go
@@ -10,7 +10,7 @@ var ApiGroupApp = new(ApiGroup)
 
 var (
 	authService    = service.NewIAuthService()
-	accessService  = service.NewIAccessService()
+	accessService  = service.NewAccessService()
 	backupService  = service.NewIBackupService()
 	settingService = service.NewISettingService()
 	logService     = service.NewILogService()
